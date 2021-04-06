8cc80749b8c1173abbb5f845cdbbdc7269c3bda3
Ibro <ibro_83@hotmail.com>
2021-04-06 11:35:30 +0200

    Create Product class

---
+++ Product.java
@@ -0,0 +1,23 @@
+
+public class Product 
+{
+	private String name;
+	
+	public Product(String name) 
+	{
+		setName("Coca Cola");
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	
+	
+	
+}
