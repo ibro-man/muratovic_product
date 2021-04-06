fd641852ad9d96ace3f143161a5c67c329b8d87c
Ibro <ibro_83@hotmail.com>
2021-04-06 18:31:22 +0200

    Company class LinkedList change, price added

--- Company.java
+++ Company.java
@@ -1,13 +1,23 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Company 
 {
-	private ArrayList<Product> products;
+	private LinkedList<Product> products;
 	private String name;
 	
 	public Company(String name)
 	{
-		products = new ArrayList<>();
+		products = new LinkedList<>();
+		setName("IBM");
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
 		this.name = name;
 	}
 }
--- Product.java
+++ Product.java
@@ -2,17 +2,25 @@
 public class Product 
 {
 	private String name;
-	
+	private float price;
+
 	public Product(String name) 
 	{
 		setName("Coca Cola");
+		setPrice(10);
 	}
-
+	public int getPrice()
+	{
+		return price;
+	}
 	public String getName()
 	{
 		return name;
 	}
-
+	public void setPrice(String price)
+	{
+		this.price = price;
+	}
 	public void setName(String name) 
 	{
 		this.name = name;
