dd58adb8e394044c70b4fd4092e7d11e3c896ea1
Benny <benjamin.sauer@gmx.net>
2021-04-06 18:34:52 +0200

    company OnStock Product TreeSet

--- Product.java
+++ Product.java
@@ -1,11 +1,16 @@
+package model;
+
+import java.util.TreeSet;
 
 public class Product 
 {
 	private String name;
+	private TreeSet<Product> products;
 	
-	public Product(String name) 
-	{
-		setName("Coca Cola");
+	public Product(String name, TreeSet<Product> products) {
+		super();
+		this.name = name;
+		this.products = products;
 	}
 
 	public String getName()
@@ -17,7 +22,13 @@ public class Product
 	{
 		this.name = name;
 	}
-	
-	
-	
+
+	public TreeSet<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(TreeSet<Product> products) {
+		this.products = products;
+	}
+		
 }
